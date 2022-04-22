df20b1efe795299b82d9cdc52c428b7c06b322e6
afroCoderHanane <gbadhanane@gmail.com>
2022-04-22 00:44:32 -0700

    .

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -2,4 +2,4 @@
 --create index on tweets_jsonb using gin((data ->'entities'->'hashtags'));
 --create index on tweets_jsonb using gin(to_tsvector('english',(data ->>'text')));
 --create index on tweets_jsonb using gin(to_tsvector('english',(data -> 'extended_tweet' ->>'full_text')));
-create index on tweets_jsonb using gin(to_tsvector('english',coalesce(data->>'text',data->'extended_tweet'->>'full_text')));
+create index on tweets_jsonb using gin(to_tsvector('english',coalesce(data->'extended_tweet'->>'full_text',data->>'text')));
--- sql.denormalized/04.sql
+++ sql.denormalized/04.sql
@@ -1,4 +1,4 @@
 SELECT count(distinct (data->>'id'))
 FROM tweets_jsonb
-WHERE to_tsvector('english',coalesce(data->>'text',data->'extended_tweet'->>'full_text'))@@to_tsquery('english','coronavirus') 
+WHERE to_tsvector('english',coalesce(data->'extended_tweet'->>'full_text',data->>'text'))@@to_tsquery('english','coronavirus') 
       and data->>'lang'='en';
